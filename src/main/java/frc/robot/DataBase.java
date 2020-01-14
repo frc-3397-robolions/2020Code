ce85a35ee2200548223fb7974ff21b138e7312fa
Robolions <robolions3397@gmail.com>
2020-01-13 19:08:01 -0600

    Testing run

---
+++ src/main/java/frc/robot/DataBase.java
@@ -0,0 +1,10 @@
+package frc.robot;
+public class DataBase{
+    // Motor PWMs
+    public static int leftFrontMotorPWM;
+    public static int leftRearMotorPWM;
+    public static int rightFrontMotorPWM;
+    public static int rightRearMotorPWM;
+    
+
+}

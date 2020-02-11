ef89838f8b15c7b9d8c720a09615e520d2caf882
Robolions <robolions3397@gmail.com>
2020-02-10 18:41:24 -0600

    DriveTrain

--- src/main/java/frc/robot/DataBase.java
+++ src/main/java/frc/robot/DataBase.java
@@ -1,10 +1,10 @@
 package frc.robot;
 public class DataBase{
     // Motor PWMs
-    public static int leftFrontMotorPWM;
-    public static int leftRearMotorPWM;
-    public static int rightFrontMotorPWM;
-    public static int rightRearMotorPWM;
+    public static int leftFrontMotorPWM = 0;
+    public static int leftRearMotorPWM = 2;
+    public static int rightFrontMotorPWM = 1;
+    public static int rightRearMotorPWM = 3;
     
 
 }

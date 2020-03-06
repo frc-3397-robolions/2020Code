03ece0828eaf8c05ac33b7cfbc7a7b92ef98f40d
frc-3397-robolions <robolions3397@gmail.com>
2020-03-05 19:11:10 -0600

    3/5/20
    Setting up

--- src/main/java/frc/robot/SmartDB.java
+++ src/main/java/frc/robot/SmartDB.java
@@ -1,11 +1,15 @@
 package frc.robot;
 
-public class SmartDB{
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.drivetrain.DriveTrainBase;
+import frc.robot.subsystems.drivetrain.DriveTrainOperator;
 
+public class SmartDB{
+    private DriveTrainBase dTB;
     public SmartDB(){
-        
+        dTB = new DriveTrainBase();
     }
     protected void execute(){
-
+        SmartDashboard.putData("LeftFrontMotor", dTB.runLeftFront(1));
     }
 }

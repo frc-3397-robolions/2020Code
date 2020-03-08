4dbd7012194a7124a0465f839d3bde6f73a816cc
frc-3397-robolions <robolions3397@gmail.com>
2020-03-08 11:51:35 -0500

    3/8/20

--- src/main/java/frc/robot/subsystems/climber/ClimberBase.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberBase.java
@@ -1,16 +1,16 @@
-
 package frc.robot.subsystems.climber;
 
-
-import edu.wpi.first.wpilibj.command.Subsystem;
-
+import edu.wpi.first.wpilibj.VictorSP;
+import frc.robot.core.RobotConstants;
 
 public class ClimberBase {
-
-   
+    private VictorSP motor1; 
+    private VictorSP motor2;
+    
 
     public ClimberBase() {
-       
+       motor1 = new VictorSP(RobotConstants.PWMConstants.CLIMBER_MOTOR_PWM1);
+       motor2 = new VictorSP(RobotConstants.PWMConstants.CLIMBER_MOTOR_PWM2);
     }
 
    
--- src/main/java/frc/robot/subsystems/colorwheel/ColorWheelOperator.java
+++ src/main/java/frc/robot/subsystems/colorwheel/ColorWheelOperator.java
@@ -12,6 +12,38 @@ public class ColorWheelOperator{
 
     }
     public void teleopRun(){
+        // if (Math.abs(oi.getLeftTrigger()) > 0){
+        // intake.runIntake(0.4, false);
+        // } else if (Math.abs(oi.getRightTrigger()) > 0){
+        // intake.runIntake(-0.4, false);
+        // } else {
+        // intake.runIntake(0, false);
+        // }
+        // if(oi.xBox().getAButton()){
+        // if(!targetColor.equalsIgnoreCase(cSensor.detectColor())){
+        // intake.runIntake(0.225, false);
+        // } else {
+
+        // if(wheelTimer.get() < 0.125){
+        // intake.runIntake(-0.8, false);
+
+        // } else {
+        // intake.runIntake(0, false);
+        // wheelTimer.reset();
+        // System.out.println("Stopped");
+        // }
+
+        // }
+
+        // } else {
+        // intake.runIntake(0, false);
+        // wheelTimer.reset();
+        // }
+        // if(oi.getBButton()){
+        // intake.runIntake(0.4, false);
+        // } else {
+        // intake.runIntake(0, false);
+        // }
 
     }
     public void testRun(){
